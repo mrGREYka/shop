0baafd1eaeaecc87fdf2419b5a83a077101ef692
mrGREYka <sergey@tyulyakov.ru>
2019-02-04 14:52:40 +0300

    pdf_file

--- views/site/index.php
+++ views/site/index.php
@@ -10,18 +10,9 @@ $this->title = 'My Yii Application';
 
         <div class="row">
             <div class="col-lg-4">
-                <h2>Квартал 1</h2>
+                <h2>Заключение ревизионной комиссии  2018</h2>
 
-                <p><a class="btn btn-default" href="/site/download?filename=file.txt">Скачать файл &raquo;</a></p>
-            </div>
-            
-        </div>
-
-        <div class="row">
-            <div class="col-lg-4">
-                <h2>Квартал 2</h2>
-
-                <p><a class="btn btn-default" href="/site/download?filename=work.xlsx">Скачать файл &raquo;</a></p>
+                <p><a class="btn btn-default" href="/site/download?filename=ZaklRevCom2018.pdf">Скачать файл &raquo;</a></p>
             </div>
             
         </div>
