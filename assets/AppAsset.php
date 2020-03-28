e1b03dcce41e490c5be91e0eaa1add6c05ef286d
mrGREYka <sergey@tyulyakov.ru>
2020-03-29 00:46:15 +0300

    style

--- assets/AppAsset.php
+++ assets/AppAsset.php
@@ -20,6 +20,7 @@ class AppAsset extends AssetBundle
     public $basePath = '@webroot';
     public $baseUrl = '@web';
     public $css = [
+        'css/admin_site.css',
         'css/site.css',
     ];
     public $js = [
--- views/site/index.php
+++ views/site/index.php
@@ -6,30 +6,24 @@ $this->title = 'My Yii Application';
 ?>
 <div class="site-index">
 
-    <div class="body-content">
+    <div class="body-content welcome-page">
 
-        <div class="row">
-            <div class="col-lg-3 col-xs-12 col-sm-6">
-                <h2>Менеджеры</h2>
-                <p><a class="btn btn-default" href="/user">Перейти &raquo;</a></p>
-            </div>
-            <div class="col-lg-3 col-xs-12 col-sm-6">
-                <h2>Партнеры</h2>
-                <p><a class="btn btn-default" href="/partner">Перейти &raquo;</a></p>
-            </div>
-            <div class="col-lg-3 col-xs-12 col-sm-6">
-                <h2>Заказы</h2>
-                <p><a class="btn btn-default" href="/order">Перейти &raquo;</a></p>
-            </div>
-            <div class="col-lg-3 col-xs-12 col-sm-6">
-                <h2>Группы товаров</h2>
-                <p><a class="btn btn-default" href="/groupproduct">Перейти &raquo;</a></p>
-            </div>
-            <div class="col-lg-3 col-xs-12 col-sm-6">
-                <h2>Товары</h2>
-                <p><a class="btn btn-default" href="/product">Перейти &raquo;</a></p>
-            </div>
-        </div>
+        <h1>Добро пожаловать, <span><?= Yii::$app->user->identity->username ?></span>.</h1>
+
+                <h2><a title="Перейти к спискам" class="btn btn-default" href="/user">Менеджеры</a></h2>                 
+
+                <h2><a title="Перейти к спискам" class="btn btn-default" href="/partner">Партнеры</a></h2> 
+                
+
+                <h2><a title="Перейти к спискам" class="btn btn-default" href="/order">Заказы</a></h2> 
+                
+
+                <h2><a title="Перейти к спискам" class="btn btn-default" href="/groupproduct">Группы товаров</a></h2> 
+                
+
+                <h2><a title="Перейти к спискам" class="btn btn-default" href="/product">Товары</a></h2> 
+                
+
 
     </div>
 </div>
