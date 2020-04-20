c203c44fcdd7a19c818248d39d2b1aba93170429
mrGREYka <sergey@tyulyakov.ru>
2020-04-20 12:09:06 +0300

    error

--- models/FileProduct.php
+++ models/FileProduct.php
@@ -40,7 +40,7 @@ class FileProduct extends \yii\db\ActiveRecord
             [['product_id'], 'integer'],
             [['title', 'filename', 'filepath', 'filename_thumb', 'filepath_thumb'], 'string', 'max' => 255],
             [['product_id'], 'exist', 'skipOnError' => true, 'targetClass' => Product::className(), 'targetAttribute' => ['product_id' => 'id']],
-            [['image'], 'file', 'extensions' => 'png, jpg', 'maxFiles' => 1, 'minSize' => 40000, 'maxSize' => 150000],
+            [['image'], 'file', 'extensions' => 'png, jpg', 'maxFiles' => 1, 'minSize' => 25000, 'maxSize' => 150000],
             [['image_thumb'], 'file', 'extensions' => 'png, jpg', 'maxFiles' => 1, 'minSize' => 5000, 'maxSize' => 40000],
             [['product_id','title',], 'required'],
             [['image','image_thumb',], 'required'],
